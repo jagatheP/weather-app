7e076af4448f00cf583183ca497aef078a500d76
Mahesh Kumar <codewithgmk@gmail.com>
2022-10-18 08:47:05 +0530

    Configured tailwindcss setup and added unicons for icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='text-center'>
+      <UilReact size='50' color='#61DAFB' className='display: inline' />
+      <h1 className='text-3xl display: inline font-bold underline'>
+        Hello world!
+      </h1>
     </div>
   );
 }
